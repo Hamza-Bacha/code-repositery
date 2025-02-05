7cd1023ccf587eb3b3f6c2a8a8844bd7cd1bec0c
HAMZA BACHA <shamzabacha03@gmail.com>
2025-02-05 00:06:38 -0800

    Update App.jsx

--- counter_project/src/App.jsx
+++ counter_project/src/App.jsx
@@ -1,79 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-
-//   let [counter,setCounter] = useState(10) 
- 
-// //  // let counter_value = 5 
-// //  const addvalue = () => {
-// //   if (counter >= 20) {
-// //     setCounter(counter + 1)
-// //  } else {
-// //   console.log("Counter cannot go above twenty")
-// //  }
-// //     //console.log("clicked",counter)
-// //    // counter_value = counter_value + 1;
-// //     //setCounter(counter+1)
-    
-// // const remove_value = () => {
-  
-// //   if (counter > 0) {
-// //     setCounter(counter - 1)
-// //   } else {
-// //     console.log("Counter cannot go below zero")
-// //   }
-
-// //   }
-
-// const addvalue = () => {
-//   if (counter < 20) {
-//     setCounter(counter + 1)
-//   } else {
-//     console.log("Counter cannot go above twenty")
-//   }
-// }
-
-// const remove_value = () => {
-//   if (counter > 0) {
-//     setCounter(counter - 1)
-//   } else {
-//     console.log("Counter cannot go below zero")
-//   }
-//  }
- 
-//   return (
-//     <>
-//     <h1>home page</h1>
-//     <p>counter value {counter}</p>
-//     <button onClick={addvalue}>add value{counter}</button>
-//     <br/>
-//     <button onClick = {remove_value}>remove{counter}</button>
-//     </>
-//   )
-// }
 function App() {
-let [counter,setCounter]= useState(15)
+  const [counter, setCounter] = useState(15);
 
-const add_value = () => {
-  setCounter(previous_counter => previous_counter +1 )
-  setCounter(previous_counter => previous_counter +1 )
-  setCounter(previous_counter => previous_counter +1 )
-}
-const remove_value = () => {
-  setCounter(counter-1)
+  const addValue = () => {
+    if (counter < Infinity) {
+      setCounter(prevCounter => prevCounter + 1);
+    } else {
+      console.log("Counter cannot go above twenty");
+    }
+  };
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(prevCounter => prevCounter - 1);
+    } else {
+      console.log("Counter cannot go below zero");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Counter Project</h1>
+      <p>Counter Value: {counter}</p>
+      <button onClick={addValue}>Add Value</button>
+      <button onClick={removeValue}>Remove Value</button>
+    </div>
+  );
 }
 
-  return(
-    <>
-    <h1>counter project</h1>
-    <p>counter value {counter}</p>
-    <button onClick ={add_value} >add value{counter}</button>
-    <button onClick={remove_value}>remove value{counter}</button>
-    
-    
-    </>
-  )
-}
-export default App
+export default App;
